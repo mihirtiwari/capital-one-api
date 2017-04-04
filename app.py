348b6502738d81d8e426c57720a80d183d49b195
Mihir Tiwari <mtiw999@gmail.com>
2017-04-03 23:01:28 -0400

    fixing heroku stuff

--- app.py
+++ app.py
@@ -238,4 +238,5 @@ def business_details(id):
     return jsonify(rating)
 
 if __name__ == "__main__":
-    app.run(debug=True,port=os.environ['PORT']);
+    port = int(os.environ.get("PORT", 5000))
+    app.run(debug=True, host="0.0.0.0", port=port)
